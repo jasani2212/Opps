87724def2f50e643ed59c0495a56b6e104a4b630
jasani2212 <nitinjasani5795@gmail.com>
2024-02-09 17:16:23 +0530

    hey you this is my First prg

---
+++ OOPS/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Inheritance
+
+class Animal
+{
+    protected void eat()
+    {
+        Console.WriteLine("heyy I can Eat");
+    }
+}
+class Dog : Animal
+{
+    static void Main(string[] args)
+    {
+
+        Dog obj = new Dog();
+
+        obj.eat();
+
+        Console.ReadLine();
+    }
+}

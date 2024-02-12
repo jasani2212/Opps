30d6efc17fa604c1dd8f7f5119718f73262761fe
jasani2212 <nitinjasani5795@gmail.com>
2024-02-12 12:19:05 +0530

    Interface Added

--- OOPS/Interface.cs
+++ OOPS/Interface.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace OOPS
 {
+    {
     interface Myinterface
     {
         void myMethod();
@@ -17,4 +18,6 @@ namespace OOPS
             Console.WriteLine("heyy how are you");
         }
     }
+}
+
 }
